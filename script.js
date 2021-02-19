1ae5e2d6248d8bf7a5ca26ede2a6a284d02eeaf3
Michael Baker <michael.a.baker34@gmail.com>
2021-02-19 12:42:51 -0600

    add randomized elements and selectors for shapes and colors

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 "use-strict";
 
+// (function () {
+
+// randomized variables
+    const randomShapes = ["square", "circle", "triangle", "rectangle", "pentagon"];
+    const randomColors = ["red", "green", "blue", "yellow", "purple"];
+    let randomShapeSelector = function randomShape(randomShapes) { 
+            return randomShapes[Math.floor(Math.random() * randomShapes.length)];
+        }
+    let randomColorSelector = function randomColor(randomColors) {
+            return randomColors[Math.floor(Math.random() * randomColors.length)];
+        }
+// html
+    let $numberOfShapes = $("#number-of-shapes-input");
+    let renderShape = [
+        {
+            "shape": "",
+            "color":  ""
+        }
+    ]
+
+    // variable console logs
+    // console.log(randomShapes);
+    // console.log(randomColors);
+    // console.log(randomShapeSelector(randomShapes));
+    // console.log(randomColorSelector(randomColors));
+    
+// });
