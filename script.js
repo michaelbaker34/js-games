d6dbc3c3d5b73c9e99491e949ab3483d84cd7c43
Michael Baker <michael.a.baker34@gmail.com>
2021-02-21 12:57:21 -0600

    completely remake memory-game

--- script.js
+++ script.js
@@ -2,39 +2,13 @@
 
 $(document).ready(function() {
 
-    let randomShapes = ["square", "circle", "triangle", "rectangle", "pentagon", "diamond"];
-    let randomColors = ["red", "green", "blue", "yellow", "purple", "orange"];
-    const randomShapeSelector = function randomShape(randomShapes) { 
-            return randomShapes[Math.floor(Math.random() * randomShapes.length)];
-        }
-    const randomColorSelector = function randomColor(randomColors) {
-            return randomColors[Math.floor(Math.random() * randomColors.length)];
+    const colors = ["red", "green", "blue", "yellow", "purple", "orange"];
+    const colorSelector = function randomColor(colors) {
+            return colors[Math.floor(Math.random() * colors.length)];
         }
     
-    let createShapeObject = [
-        {
-            "shape": randomShapeSelector(randomShapes),
-            "color": randomColorSelector(randomColors)
-        }
-    ]
-    let $numberOfShapes = $("#number-of-shapes-input");
-
-    console.log(createShapeObject);
-
-    $("#submit-number-of-shapes").click((e) => {
-        console.log($numberOfShapes.val())
-        let $numberOfShapesInput = $numberOfShapes.val();
-        
-        for (let i = 0; i < $numberOfShapesInput; i++) {
-            function renderShapeObjects(i) {
-                i.forEach((value) => {
-                    let $shapeDiv = $("<div>",{id:'shape' + i});
-                    console.log($shapeDiv);
-                });
-            }
-            
-        }
-
+    $("#game-start").click((e) => {
+        console.log(colorSelector);
     });
 
 });
