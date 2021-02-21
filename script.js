153db2fce3c89ae31204bb53bc9655331c2c0eb6
Michael Baker <michael.a.baker34@gmail.com>
2021-02-21 10:13:39 -0600

    add retrieve input on click function

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 "use-strict";
 
-// (function () {
+$(document).ready(function() {
 
-// randomized variables
-    const randomShapes = ["square", "circle", "triangle", "rectangle", "pentagon"];
-    const randomColors = ["red", "green", "blue", "yellow", "purple"];
+    const randomShapes = ["square", "circle", "triangle", "rectangle", "pentagon", "diamond"];
+    const randomColors = ["red", "green", "blue", "yellow", "purple", "orange"];
     let randomShapeSelector = function randomShape(randomShapes) { 
             return randomShapes[Math.floor(Math.random() * randomShapes.length)];
         }
     let randomColorSelector = function randomColor(randomColors) {
             return randomColors[Math.floor(Math.random() * randomColors.length)];
         }
-// html
-    let $numberOfShapes = $("#number-of-shapes-input");
+    
     let renderShape = [
         {
             "shape": randomShapeSelector(randomShapes),
@@ -20,10 +18,11 @@
         }
     ]
 
-    // console.log(randomShapes);
-    // console.log(randomColors);
-    // console.log(randomShapeSelector(randomShapes));
-    // console.log(randomColorSelector(randomColors));
     console.log(renderShape);
 
-// });
+    $("#submit-number-of-shapes").click(function () {
+        let $numberOfShapes = $("#number-of-shapes-input").val();
+        console.log($numberOfShapes)
+    });
+
+});
